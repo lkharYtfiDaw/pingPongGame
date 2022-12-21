80c7188cbbaf816d0bb197f540fbb6076907940f
Mohamed El hadjaoui <mel-hadj@e1r10p7.1337.ma>
2022-12-21 23:18:03 +0100

    working on form in profile

--- src/components/view/Profile/ProfileCard.tsx
+++ src/components/view/Profile/ProfileCard.tsx
@@ -4,13 +4,51 @@ import ProfileImage from './ProfileImage'
 export function ProfileCard() {
   return (
     <div className='col-span-2 bg-[#242424] ring-2 ring-gray-600 ring-offset-1 rounded-md w-full  flex flex-col items-center justify-evenly  px-4 pt-12 pb-6'>
-     <ProfileImage/>
-      <div id='title' className='font-semibold text-center text-xl m-4'>Mohamed El Hadjaoui</div>
-      <div id='subtitle' className='text-sm text-gray-200'>Web3 Developer</div>
+      <div className='flex flex-col items-center justify-between ring-2 ring-gray-600 ring-offset-1 rounded-md px-12 pt-12 pb-6 shadow-lg shadow-slate-700 w-3/4 space-y-8' >
+        <ProfileImage />
+        <div className="mb-3 w-96">
+          <label htmlFor="formFile" className="form-label inline-block mb-2">Choose your Image</label>
+          <input className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
+    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="formFile" />
+        </div>
+        <div className="flex justify-center">
+          <div className="mb-3 xl:w-96">
+            <label htmlFor='exampleText0' className="form-label inline-block mb-2 ">Name</label>
+            <input
+              type="text"
+              className=" form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
+                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleText0"
+              placeholder="Mohamed El Hadjaoui"
+            />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="mb-3 xl:w-96">
+            <label className="form-label inline-block mb-2 ">About You</label>
+            <textarea
+              className=" form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlTextarea1"
+              rows={3}
+          onChange={(e) => {
+  
+                
+          }}
+              placeholder="Your message"
+            ></textarea>
+          </div>
+        </div>
 
-      <div id='title'>Actions</div>
+        <div className="flex justify-center">
 
-      <Acheivements/>
+        </div>
+
+
+      </div>
+
+
+      <Acheivements />
 
     </div>
   )
--- src/components/view/Profile/ProfileImage.tsx
+++ src/components/view/Profile/ProfileImage.tsx
@@ -4,8 +4,8 @@ import avtar from '../../../assets/avatar.jpeg'
 function ProfileImage() {
     return (
         <div className='relative '>
-            <div className='sm:h-4 sm:w-4 h-2 w-2  bg-green-500 absolute top-4 sm:right-1 right-0 ring-white ring-4 rounded-full'></div>
-            <img src={avtar} alt="avatar" className='sm:h-32 h-16 rounded-full ring-2 ring-offset-2 cursor-pointer' />
+            <div className='sm:h-4 sm:w-4 h-2 w-2  bg-green-500 absolute top-4  sm:right-1 right-0 ring-white ring-4 rounded-full'></div>
+            <img src={avtar} alt="avatar" className='sm:h-32 h-16 rounded-full ring-2 ring-offset-2 cursor-pointer shadow-lg shadow-gray-700' />
         </div>
     )
 }
